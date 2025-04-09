f1bb0378e0fbee89ac77eb9016505ac23c2232b6
MaxgAtenea <magomezs@agenciaatenea.gov.co>
2025-04-09 11:14:42 -0500

    Modificacion a src.ipynb

---
+++ src/constants.py
@@ -0,0 +1,47 @@
+cols_saberpro = [
+    "PERIODO",
+    "PUNT_GLOBAL",
+    "ESTU_CONSECUTIVO",
+    "ESTU_COD_MCPIO_PRESENTACION",
+    "INST_COD_INSTITUCION",
+    "INST_NOMBRE_INSTITUCION",
+    "ESTU_PRGM_ACADEMICO",
+    "ESTU_SNIES_PRGMACADEMICO",
+    "ESTU_PRGM_CODMUNICIPIO",
+    "ESTU_PRGM_MUNICIPIO",
+    "ESTU_PRGM_DEPARTAMENTO",
+    "ESTU_NIVEL_PRGM_ACADEMICO",  # UNIVERSITARIO, TECNOLOGIA, TECNICO PROFESIONAL, ESCUELA NORMAL SUPERIOR
+    "ESTU_NUCLEO_PREGRADO",
+    "ESTU_INST_CODMUNICIPIO",
+    "ESTU_INST_MUNICIPIO",
+    "ESTU_INST_DEPARTAMENTO",
+    "INST_CARACTER_ACADEMICO"  # UNIVERSIDAD, INSTITUCION UNIVERSITARIA, INSTITUCION TECNOLOGICA, TECNICA PROFESIONAL..
+]
+
+cols_sb11before_20142 = [
+    "periodo",
+    "estu_consecutivo",
+    "cole_cod_depto_ubicacion",
+    "cole_cod_mcpio_ubicacion",
+    "cole_depto_ubicacion",
+    "recaf_punt_sociales_ciudadanas",
+    "recaf_punt_ingles",
+    "recaf_punt_lectura_critica",
+    "recaf_punt_matematicas",
+    "recaf_punt_c_naturales"
+]
+
+
+cols_sb11after_20142 = [
+    "periodo",
+    "estu_consecutivo",
+    "cole_cod_depto_ubicacion",
+    "cole_cod_mcpio_ubicacion",
+    "cole_depto_ubicacion",
+    "punt_global",
+    "punt_sociales_ciudadanas",
+    "punt_ingles",
+    "punt_lectura_critica",
+    "punt_matematicas",
+    "punt_c_naturales"
+]
