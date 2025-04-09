86b64c3f00051e83b5d3fb7c3eb1862df52c8e46
MaxgAtenea <magomezs@agenciaatenea.gov.co>
2025-04-09 12:41:29 -0500

    Se incluye la informacion del Saber Pro desde el 2016 hasta el registro mas reciente

--- src/src.R
+++ src/src.R
@@ -1,45 +1,256 @@
 library(readr) # activa el paquete ya presente en la librería, para importar .txt & .csv
 library(plyr)  #activamos el paquete
 library(nlme) #activamos la librería/paquete que nos permite estimar el modelo multinivel
+library(dplyr)
 
+
+
+#Mirar el directorio de trabajo
 getwd()
 
+#Definir las variables de interes del saber pro
 cols_saberpro <- c(
-  "PERIODO",
-  "PUNT_GLOBAL",
-  "ESTU_CONSECUTIVO",
-  "ESTU_COD_MCPIO_PRESENTACION",
-  "INST_COD_INSTITUCION",
-  "INST_NOMBRE_INSTITUCION",
-  "ESTU_PRGM_ACADEMICO",
-  "ESTU_SNIES_PRGMACADEMICO",
-  "ESTU_PRGM_CODMUNICIPIO",
-  "ESTU_PRGM_MUNICIPIO",
-  "ESTU_PRGM_DEPARTAMENTO",
-  "ESTU_NIVEL_PRGM_ACADEMICO",  # UNIVERSITARIO, TECNOLOGIA, TECNICO PROFESIONAL, ESCUELA NORMAL SUPERIOR
-  "ESTU_NUCLEO_PREGRADO",
-  "ESTU_INST_CODMUNICIPIO",
-  "ESTU_INST_MUNICIPIO",
-  "ESTU_INST_DEPARTAMENTO",
-  "INST_CARACTER_ACADEMICO"  # UNIVERSIDAD, INSTITUCION UNIVERSITARIA, INSTITUCION TECNOLOGICA, TECNICA PROFESIONAL..
+  "periodo",
+  "estu_genero",
+  "estu_consecutivo",
+  "punt_global",
+  "inst_cod_institucion",
+  "estu_cod_mcpio_presentacion",
+  "inst_nombre_institucion",
+  "estu_prgm_academico",
+  "estu_snies_prgmacademico",
+  "estu_prgm_codmunicipio",
+  "estu_prgm_municipio",
+  "estu_prgm_departamento",
+  "estu_nivel_prgm_academico",
+  "estu_nucleo_pregrado",
+  "estu_inst_codmunicipio",
+  "estu_inst_municipio",
+  "estu_inst_departamento",
+  "inst_caracter_academico",
+  "mod_razona_cuantitat_punt",
+  "mod_lectura_critica_punt",
+  "mod_competen_ciudada_punt",
+  "mod_ingles_punt",
+  "mod_comuni_escrita_punt"
 )
 
-# Define the directory containing the files
+
+
+
+
+
+#Ruta del directorio con los archivos
 data_dir <- "ICFES/data/SABERPRO_raw_reduced/"
 
-sbpro_20231 <- read_delim(paste0(data_dir,"saberpro_20231.txt"),
-                         delim = "¬", escape_double = FALSE, col_select= cols_saberpro,trim_ws = TRUE)
+sbpro_20232 <- tryCatch(
+  read_delim(paste0(data_dir, "saberpro_20232.txt"),
+             delim = ";",
+             escape_double = FALSE,
+             col_select = all_of(cols_saberpro),
+             trim_ws = TRUE),
+  error = function(e) {
+    read_delim(paste0(data_dir, "saberpro_20232.txt"),
+               delim = ";",
+               escape_double = FALSE,
+               col_select = all_of(toupper(cols_saberpro)),
+               trim_ws = TRUE)
+  }
+)
+  
+sbpro_20231 <- tryCatch(
+  read_delim(paste0(data_dir, "saberpro_20231.txt"),
+             delim = "¬",
+             escape_double = FALSE,
+             col_select = all_of(cols_saberpro),
+             trim_ws = TRUE),
+  error = function(e) {
+    read_delim(paste0(data_dir, "saberpro_20231.txt"),
+               delim = "¬",
+               escape_double = FALSE,
+               col_select = all_of(toupper(cols_saberpro)),
+               trim_ws = TRUE)
+  }
+)
 
-sbpro_20221 <- read_delim(paste0(data_dir,"saberpro_20221.txt"),
-                         delim = "¬", escape_double = FALSE, col_select= cols_saberpro, trim_ws = TRUE)
 
-sbpro <- rbind.fill(sbpro_20231, sbpro_20221) 
+sbpro_20222 <- tryCatch(
+  read_delim(paste0(data_dir, "saberpro_20222.txt"),
+             delim = "¬",
+             escape_double = FALSE,
+             col_select = all_of(cols_saberpro),
+             trim_ws = TRUE),
+  error = function(e) {
+    read_delim(paste0(data_dir, "saberpro_20222.txt"),
+               delim = "¬",
+               escape_double = FALSE,
+               col_select = all_of(toupper(cols_saberpro)),
+               trim_ws = TRUE)
+  }
+)
+
+sbpro_20221 <- tryCatch(
+  read_delim(paste0(data_dir, "saberpro_20221.txt"),
+             delim = "¬",
+             escape_double = FALSE,
+             col_select = all_of(cols_saberpro),
+             trim_ws = TRUE),
+  error = function(e) {
+    read_delim(paste0(data_dir, "saberpro_20221.txt"),
+               delim = "¬",
+               escape_double = FALSE,
+               col_select = all_of(toupper(cols_saberpro)),
+               trim_ws = TRUE)
+  }
+)
+
+
+
+sbpro_2021 <- tryCatch(
+  read_delim(paste0(data_dir, "saberpro_2021.txt"),
+             delim = "¬",
+             escape_double = FALSE,
+             col_select = all_of(cols_saberpro),
+             trim_ws = TRUE),
+  error = function(e) {
+    read_delim(paste0(data_dir, "saberpro_2021.txt"),
+               delim = "¬",
+               escape_double = FALSE,
+               col_select = all_of(toupper(cols_saberpro)),
+               trim_ws = TRUE)
+  }
+)
+
+sbpro_2020 <- tryCatch(
+  read_delim(paste0(data_dir, "saberpro_gen_2020.txt"),
+             delim = "¬",
+             escape_double = FALSE,
+             col_select = all_of(cols_saberpro),
+             trim_ws = TRUE),
+  error = function(e) {
+    read_delim(paste0(data_dir, "saberpro_gen_2020.txt"),
+               delim = "¬",
+               escape_double = FALSE,
+               col_select = all_of(toupper(cols_saberpro)),
+               trim_ws = TRUE)
+  }
+)
+
+sbpro_2019 <- tryCatch(
+  read_delim(paste0(data_dir, "saberpro_gen_2019.txt"),
+             delim = "¬",
+             escape_double = FALSE,
+             col_select = all_of(cols_saberpro),
+             trim_ws = TRUE),
+  error = function(e) {
+    read_delim(paste0(data_dir, "saberpro_gen_2019.txt"),
+               delim = "¬",
+               escape_double = FALSE,
+               col_select = all_of(toupper(cols_saberpro)),
+               trim_ws = TRUE)
+  }
+)
+
+sbpro_2018 <- tryCatch(
+  read_delim(paste0(data_dir, "saberpro_gen_2018.txt"),
+             delim = "¬",
+             escape_double = FALSE,
+             col_select = all_of(cols_saberpro),
+             trim_ws = TRUE),
+  error = function(e) {
+    read_delim(paste0(data_dir, "saberpro_gen_2018.txt"),
+               delim = "¬",
+               escape_double = FALSE,
+               col_select = all_of(toupper(cols_saberpro)),
+               trim_ws = TRUE)
+  }
+)
+
+sbpro_2017 <- tryCatch(
+  read_delim(paste0(data_dir, "saberpro_gen_2017.txt"),
+             delim = "¬",
+             escape_double = FALSE,
+             col_select = all_of(cols_saberpro),
+             trim_ws = TRUE),
+  error = function(e) {
+    read_delim(paste0(data_dir, "saberpro_gen_2017.txt"),
+               delim = "¬",
+               escape_double = FALSE,
+               col_select = all_of(toupper(cols_saberpro)),
+               trim_ws = TRUE)
+  }
+)
+
+sbpro_2016 <- tryCatch(
+  read_delim(paste0(data_dir, "saberpro_gen_2016.txt"),
+             delim = "¬",
+             escape_double = FALSE,
+             col_select = all_of(cols_saberpro),
+             trim_ws = TRUE),
+  error = function(e) {
+    read_delim(paste0(data_dir, "saberpro_gen_2016.txt"),
+               delim = "¬",
+               escape_double = FALSE,
+               col_select = all_of(toupper(cols_saberpro)),
+               trim_ws = TRUE)
+  }
+)
+
+for (name in c("sbpro_20232", "sbpro_20231", "sbpro_20222", "sbpro_20221",
+               "sbpro_2021", "sbpro_2020", "sbpro_2019", "sbpro_2018",
+               "sbpro_2017", "sbpro_2016")) {
+  df <- get(name)
+  names(df) <- tolower(names(df))
+  assign(name, df)
+}
+
+for (name in c("sbpro_20232", "sbpro_20231", "sbpro_20222", "sbpro_20221",
+               "sbpro_2021", "sbpro_2020", "sbpro_2019", "sbpro_2018",
+               "sbpro_2017", "sbpro_2016")) {
+  df <- get(name)
+  df$inst_cod_institucion <- as.integer(df$inst_cod_institucion)
+  df$estu_cod_mcpio_presentacion <- as.integer(df$estu_cod_mcpio_presentacion)
+  df$estu_prgm_codmunicipio <- as.integer(df$estu_prgm_codmunicipio)
+  df$estu_inst_codmunicipio <- as.integer(df$estu_inst_codmunicipio)
+  assign(name, df)
+}
+
+remove(df)
+
+sbpro <- bind_rows(
+  sbpro_20232,
+  sbpro_20231,
+  sbpro_20222,
+  sbpro_20221,
+  sbpro_2021,
+  sbpro_2020,
+  sbpro_2019,
+  sbpro_2018,
+  sbpro_2017,
+  sbpro_2016
+  ) 
+
 names (sbpro)=tolower(names(sbpro))
-remove(sbpro_20231, sbpro_20221) ## elimino las BD que no voy a emplear
-table (sbpro$periodo)
+
+#filtrar por los programas que se ofrecen en bogota
+temp_sbpro <- sbpro %>%filter(estu_prgm_codmunicipio == 11001)
+
+remove(
+  sbpro_20232,
+  sbpro_20231,
+  sbpro_20222,
+  sbpro_20221,
+  sbpro_2021,
+  sbpro_2020,
+  sbpro_2019,
+  sbpro_2018,
+  sbpro_2017,
+  sbpro_2016
+) 
+
 
 # Save data frame as a CSV file
-write.csv(sbpro, "ICFES/data/SABERPRO_cleaned/base_sbpro.csv", row.names = FALSE)  # row.names = FALSE excludes row numbers
+write.csv(temp_sbpro, "ICFES/data/SABERPRO_cleaned/base_sbpro.csv", row.names = FALSE)  # row.names = FALSE excludes row numbers
 
 data <- read_delim("ICFES/data/BD/bd.csv", escape_double = FALSE, trim_ws = TRUE)
 
