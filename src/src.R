20315edb0a57eafafa88b4a69763d8a98a9d5e34
MaxgAtenea <magomezs@agenciaatenea.gov.co>
2025-04-11 16:05:42 -0500

    Regresion multinivel v01: Se hacen cambios menores en los datos y se cambia el cine detallado por el cine especifico para la regresion multinivel

--- src/src.R
+++ src/src.R
@@ -1,20 +1,22 @@
 ############################################################
 #Calculo valor agregado
 #Abril 2025 
-#Atenea
+#Autor:TBD
 
 #Fuentes de los datos
 
 #1.bd.csv: Base elaborada manualmente con la informacion de data icfes.  
 
 #2. codigos_snies_cine_2023.csv: https://snies.mineducacion.gov.co/portal/ESTADISTICAS/Bases-consolidadas/
-#Archivo csv: "Estudiantes matriculados 2023"
-#El archivo viene sin duplicados de codigos snies
+#Archivo csv: "Estudiantes matriculados 2023.csv"
+#NOTA: El archivo viene sin duplicados de los codigos SNIES
+
 ############################################################
 
 
 #Cargar las librerias
 library(readr) #lectura de archivos
+library(stringi) #para ajustar nombres de las columnas de los data frames
 library(dplyr) #manipulacion de dataframes
 library(tidyr)
 library(ggplot2) #para graficar
@@ -22,10 +24,29 @@ library(nlme) #activamos la librería/paquete que nos permite estimar el modelo 
 
 
 ##################################
+#constantes
+##################################
+
+variables_cine_snies <- c(
+  "codigo_snies_del_programa",
+  "programa_academico",
+  "nucleo_basico_del_conocimiento_nbc",
+  "id_cine_campo_amplio",
+  "id_cine_campo_especifico",
+  "id_cine_campo_detallado",
+  "codigo_del_municipio_programa",
+  "codigo_de_la_institucion",
+  "institucion_de_educacion_superior_ies",
+  "ies_acreditada",
+  "caracter_ies"
+)
+
+
+##################################
 #Lectura de los datos
 ##################################
 
-#Leer la base consolidada i,e., Saber Pro cruzado con Saber 11
+#Leer la base consolidada del Saber Pro cruzado con Saber 11
 icfes <- read_delim("ICFES/data/BD/bd.csv", escape_double = FALSE, trim_ws = TRUE)
 
 
@@ -36,59 +57,51 @@ cine_snies <- read_delim("ICFES/data/SNIES_CINE/codigos_snies_cine_2023.csv",
                          delim=";",
                          locale = locale(encoding = "Latin1"))
 
-#Verificacion de duplicados para cine_snies
-keys <- c(
-  "PROGRAMA ACADÉMICO",
-  "PROGRAMA ACREDITADO",
-  "CÓDIGO DE LA INSTITUCIÓN",
-  "CÓDIGO DEL MUNICIPIO (PROGRAMA)",
-  "ID MODALIDAD"
-  #"ID SEXO",
-  #"SEMESTRE",
-  #"MATRICULADOS"
-  )
 
-#Si bien el codigo SNIES es unico, el nombre del Programa Académico tiene duplicados
-#Estos estan en funcion de:
-#1. Programa academico
-#2. Programa acreditado
-#3. Codigo de la institucion
-#4. CÓDIGO DEL MUNICIPIO (PROGRAMA)
-#5. Id modalidad 
-#6. Id sexo #no es relevante para el programa
-#7. Semestre #no es relevante para el programa
-#8. Matriculados #no es relevante para el programa
-#Existen 73 duplicados con las llaves del 1-5 y 14 duplicados del 1-8
+#Limpiar el nombre de las columnas de cine_sines
+names(cine_snies) <- names(cine_snies) %>%
+  stri_trans_general("Latin-ASCII") %>%       # elimina tildes
+  tolower() %>%                                # convierte a minúsculas
+  gsub(" ", "_", .) %>%                        # reemplaza espacios con _
+  gsub("\\(|\\)", "", .)   
 
-# #Mirar cuales son las filas duplicadas
-# duplicated_rows <- cine_snies %>%
-#   filter(duplicated(select(., all_of(keys))))
-
-#Quedarse con las filas no duplicadas
 cine_snies <- cine_snies %>%
-  distinct(across(all_of(keys)), .keep_all = TRUE)
+  select(all_of(variables_cine_snies))
+
+#De todas maneras verificamos que no hayan duplicados de codigos SNIES
+#El numero de valores unicos coincide con el numero de observaciones en cine_snies
+cine_snies %>%
+  summarise(valores_unicos = n_distinct(codigo_snies_del_programa))
+
+#Mirar cuantos valores unicos hay para los nucleos basicos del conocimiento
+#en el dataframe icfes
+#100 valores unicos
+icfes %>%
+  summarise(valores_unicos = n_distinct(estu_nucleo_pregrado))
 
 #Filtrar por los programas que se ofrecen en Bogota
 #Tener en cuenta que existe otro potencial filtro: `CÓDIGO DEL MUNICIPIO IES`
+#Sin embargo, para la estimacion el ICFES distingue las regiones donde se ofrece el programa
 #Discutirlo con el equipo
-
 cine_snies <- cine_snies %>%
-  filter(`CÓDIGO DEL MUNICIPIO (PROGRAMA)` == 11001)
+  filter(codigo_del_municipio_programa == 11001)
+
 
 ############################################
 #Mirar los programas cuyos snies no cruzan
 ############################################
 
-#Miramos los codigos snies de los programas en la BD cine_snies
+
+#Miramos los codigos SNIES de los programas en la el dataframe cine_snies
 programas_bdcine_snies <- cine_snies %>%
   select(
-    codigo_snies = `CÓDIGO SNIES DEL PROGRAMA`,
-    nombre_programa_bdsnies = `PROGRAMA ACADÉMICO`
+    codigo_snies = codigo_snies_del_programa,
+    nombre_programa_bdsnies = programa_academico
   ) %>%
   distinct() %>%
   mutate(from_cine_snies = TRUE) #agrega identificador de la BD donde vienen los datos
 
-#Miramos codigos snies de los programas en la BD icfes
+#Miramos codigos snies de los programas en la el dataframe icfes
 programas_bdicfes <- icfes %>%
   select(
     codigo_snies = estu_snies_prgmacademico,
@@ -124,8 +137,11 @@ programas_matching <- programas %>%
 data <- inner_join(
   icfes,
   cine_snies,
-  by = join_by(estu_snies_prgmacademico==`CÓDIGO SNIES DEL PROGRAMA`)
+  by = join_by(estu_snies_prgmacademico==codigo_snies_del_programa)
 )
+
+#Crear la columna de INBC
+#TO DO
 
 #liberamos memoria
 remove(cine_snies)
@@ -144,6 +160,15 @@ data_summary <- data %>%
     names_sep = "___"
   )
 
+############################################
+#Estadisticas descriptivas de las variables
+############################################
+
+
+##################################
+#Frecuencia SNIES
+##################################
+
 #Mirar la frecuencia de cada programa
 #Nota: recordar que el programa depende de la institucion.
 programas_freq <- data %>%
@@ -161,17 +186,41 @@ ggplot(top_10, aes(x = reorder(programa_label, frecuencia), y = frecuencia)) +
   labs(title = "Top 10 Programas", x = "Programa", y = "Frecuencia") +
   theme_minimal()
 
-# bottom_10 <- programas_freq %>% slice_min(frecuencia, n = 10)
-# # Bottom 10
-# ggplot(bottom_10, aes(x = reorder(programa_label, frecuencia), y = frecuencia)) +
-#   geom_bar(stat = "identity", fill = "firebrick") +
-#   coord_flip() +
-#   labs(title = "Bottom 10 Programas", x = "Programa", y = "Frecuencia") +
-#   theme_minimal()
-
 #Graficar una densidad de la frecuencia de los programas
 #La grafica se utiliza para tener un panorama general de las frecuencias
 ggplot(programas_freq, aes(x = frecuencia)) +
+  geom_density(fill = "skyblue", alpha = 0.6) +
+  labs(
+    x = "Frecuencia de aparición del programa",
+    y = "Densidad estimada",
+    title = "Densidad de frecuencias de programas"
+  ) +
+  theme_minimal()
+
+##################################
+#Frecuencia CINE Especifico
+##################################
+
+#Mirar la frecuencia de cada cine especifico
+cine_freq <- data %>%
+  count(id_cine_campo_especifico, name = "frecuencia")
+
+#Obtener los top 10 programas con mas frecuencia
+top_10_cine <- cine_freq %>% slice_max(frecuencia, n = 10)
+
+ggplot(top_10_cine, aes(x = reorder(id_cine_campo_especifico, frecuencia), y = frecuencia)) +
+  geom_col(fill = "#69b3a2") +
+  coord_flip() +
+  geom_text(aes(label = frecuencia), hjust = -0.1, size = 4) +
+  labs(title = "Top 10: Códigos CINE con mayor frecuencia",
+       x = "ID CINE Campo Específico",
+       y = "Frecuencia") +
+  theme_minimal()
+
+
+#Graficar una densidad de la frecuencia de los programas
+#La grafica se utiliza para tener un panorama general de las frecuencias
+ggplot(cine_freq, aes(x = frecuencia)) +
   geom_density(fill = "skyblue", alpha = 0.6) +
   labs(
     x = "Frecuencia de aparición del programa",
@@ -184,27 +233,10 @@ ggplot(programas_freq, aes(x = frecuencia)) +
 #Data para el calcaculo del VA
 ############################################
 
-
-#Seleccionar las columnas de interes
-keys <- c(
-  "estu_consecutivo_icfes",
-  "estu_consecutivo_pro",
-  "INSTITUCIÓN DE EDUCACIÓN SUPERIOR (IES)",
-  "inst_nombre_institucion",
-  "estu_nucleo_pregrado",
-  "estu_snies_prgmacademico",
-  "NÚCLEO BÁSICO DEL CONOCIMIENTO (NBC)",
-  "ID CINE CAMPO AMPLIO",
-  "ID CINE CAMPO ESPECIFICO",
-  "ID CINE CAMPO DETALLADO",
-  "punt_global_pro",
-  "punt_global_icfes",
-  "dif_periodos"
-)
-
-keys_limpios <- c(
-  "estu_consecutivo_icfes",
-  "estu_consecutivo_pro",
+#Seleccionar columnas de interes
+columnas_regresion <- c(
+  "estu_consecutivo_bdsaber11",
+  "estu_consecutivo_bdsaberpro",
   "institucion_de_educacion_superior_ies",
   "inst_nombre_institucion",
   "estu_nucleo_pregrado",
@@ -213,49 +245,29 @@ keys_limpios <- c(
   "id_cine_campo_amplio",
   "id_cine_campo_especifico",
   "id_cine_campo_detallado",
-  "punt_global_pro",
-  "punt_global_icfes",
+  "punt_global_bdsaberpro",
+  "punt_global_bdsaber11",
   "dif_periodos"
 )
 
 #Seleccionar las anteriores variables y quedarnos con las filas que si tienen
 #observaciones en punt_global_icfes
 data_filtrado <- data %>%
-  select(all_of(keys)) %>%
+  select(all_of(columnas_regresion)) %>%
   filter(
-    !is.na(punt_global_icfes),
+    !is.na(punt_global_bdsaber11),
     dif_periodos >= 40,
     dif_periodos <= 80,
     estu_nucleo_pregrado != "MEDICINA"
   )
 
-#renombrar las columnas
-colnames(data_filtrado) <- keys_limpios
 
-#Existen 54 NBC unicos
+#Existen 54 NBC unicos (recuerdar que se omitio medicina)
 #El problema con el campo NBC es que no es un id sino un string, entonces no es fiable
 length(unique(data_filtrado$nucleo_basico_del_conocimiento_nbc))
 
 #Existen 72 CINE detallados unicos
 length(unique(data_filtrado$id_cine_campo_detallado))
-
-#Conteo de observaciones por codigo cine_detallado
-conteo_campo_detallado <- data_filtrado %>%
-  count(id_cine_campo_detallado, name = "frecuencia")
-
-#Grafica de barras para visualizar el conteo por codigo cine detallado
-ggplot(conteo_campo_detallado, aes(x = reorder(id_cine_campo_detallado, -frecuencia), y = frecuencia)) +
-  geom_bar(stat = "identity", fill = "steelblue") +
-  geom_text(aes(label = frecuencia), vjust = -0.2, angle = 90, size = 3) +
-  labs(
-    title = "Frecuencia por ID CINE Campo Detallado",
-    x = "ID CINE Campo Detallado",
-    y = "Frecuencia"
-  ) +
-  theme_minimal() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
-
-
 
 ############################################
 #Data para el calcaculo del VA
@@ -277,16 +289,16 @@ attach(data_filtrado)
 
 #estimamos la regresion multinivel con los codigos CINE
 multinivel_basico <- lme(
-  punt_global_pro ~ punt_global_icfes, random = ~ 1| id_cine_campo_detallado, data = data_filtrado
+  punt_global_bdsaberpro ~ punt_global_bdsaber11, random = ~ 1| id_cine_campo_especifico, data = data_filtrado
   )
 
-#Ahora extraemos el listado de efectos aleatorios (valor agregado)
+
+#extraemos el listado de efectos aleatorios (valor agregado)
+summary(multinivel_basico)
 coeff_va <- ranef(multinivel_basico)
-View(coeff_va)
 
-multinivel_basico[["coefficients"]][["random"]] # para visualizar el VA más directamente
-
-
+#visualizar el VA más directamente
+multinivel_basico[["coefficients"]][["random"]] 
 
 
 ####################################################
